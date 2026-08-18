5d4c3605d052d84d1d9778e4c3fc3af5d5a5364a
Blanca Pérez Coll <blancaperezcoll@gmail.com>
2026-08-18 11:33:06 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,28 @@
+const CACHE_NAME = 'fitstreak-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json'
+];
+
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open('fitstreak-store').then((cache) => cache.addAll([
-      './',
-      './index.html',
-      './style.css',
-      './app.js',
-      './manifest.json'
-    ]))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
